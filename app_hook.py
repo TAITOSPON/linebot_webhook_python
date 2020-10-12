e6aa199b51230fa7ddd2e5387d315890e00e01df
TAITOSPON <totsapon_p@thaitobacco.or.th>
2020-10-12 10:52:56 +0700

    commit nueng ResponsMenu

--- app_hook.py
+++ app_hook.py
@@ -6,6 +6,7 @@ from flask import Flask,request,render_template
 
 from python.ResponsListItem import ResponsListItem
 from python.ResponsNotLogin import ResponsNotLogin
+from python.ResponsMenu import ResponsMenu
 from python.ResponsQuickReply import ResponsQuickReply
 from python.RequestGet import RequestGet
 # from python.PostToDialog import PostToDialog
@@ -15,16 +16,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('login.html')
+  return "THIS LINEBOT WEBHOOK SERVER!"
   
 @app.route('/webhook' , methods = ['POST']) 
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    res = processRequest(req)
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+  req = request.get_json(silent=True, force=True)
+  res = processRequest(req)
+  res = json.dumps(res, indent=4)
+  r = make_response(res)
+  r.headers['Content-Type'] = 'application/json'
+  return r
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -44,42 +45,41 @@ def callback():
     
 
 def sendText(user,text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer 4d7QOg7qteXxTxhGEQ5ROBfc2wiBVyRTAnbA73hrZcsWLM7etaAcqpP/IS+Pv5/Psxa2nxyeSrvww7NrsRnl4n4i2Edzk36Dr5wzQZIItg1paczCVHU+/LnIEz27U68OrJSTiDooQf0xHZRx2FTp5gdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-    headers = {
-    'Content-Type': 'application/json; charset=UTF-8',
-    'Authorization':Authorization}
-    data = json.dumps({
-    "replyToken":user,
-    "messages":[{"type":"text","text":text}]})
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
+  Authorization = 'Bearer 4d7QOg7qteXxTxhGEQ5ROBfc2wiBVyRTAnbA73hrZcsWLM7etaAcqpP/IS+Pv5/Psxa2nxyeSrvww7NrsRnl4n4i2Edzk36Dr5wzQZIItg1paczCVHU+/LnIEz27U68OrJSTiDooQf0xHZRx2FTp5gdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+  headers = {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Authorization':Authorization}
+  data = json.dumps({
+  "replyToken":user,
+  "messages":[{"type":"text","text":text}]})
 
 
-    r = requests.post(LINE_API, headers=headers, data=data) 
-    #print(r.text)
+  r = requests.post(LINE_API, headers=headers, data=data) 
+  #print(r.text)
 
 
 
 def processRequest(req):
-    # Parsing the POST request body into a dictionary for easy access.
-    req_dict = json.loads(request.data)
-    print(req_dict)
-    # Accessing the fields on the POST request boduy of API.ai invocation of the webhook
-    intent = req_dict["queryResult"]["intent"]["displayName"]
-    uuid = req_dict["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
+  # Parsing the POST request body into a dictionary for easy access.
+  req_dict = json.loads(request.data)
+  print(req_dict)
+  # Accessing the fields on the POST request boduy of API.ai invocation of the webhook
+  intent = req_dict["queryResult"]["intent"]["displayName"]
+  uuid = req_dict["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
 
-    speech = str(req_dict)
-    res = makeWebhookResult(speech)
+  speech = str(req_dict)
+  res = makeWebhookResult(speech)
 
-    return res
+  return res
 
 
 def makeWebhookResult(speech):
 
-    return {"fulfillmentText": speech}
+  return {"fulfillmentText": speech}
 
 
 if __name__ == '__main__':
   # app.run(debug=True)
-
   app.run(debug=False, port='5557', host='172.20.55.46', threaded=True)
 
---
+++ python/ResponsMenu.py
@@ -0,0 +1,178 @@
+
+import requests
+import json
+
+class ResponsMenu:
+    
+    def __init__(self,devicetoken):
+        serverToken = '4d7QOg7qteXxTxhGEQ5ROBfc2wiBVyRTAnbA73hrZcsWLM7etaAcqpP/IS+Pv5/Psxa2nxyeSrvww7NrsRnl4n4i2Edzk36Dr5wzQZIItg1paczCVHU+/LnIEz27U68OrJSTiDooQf0xHZRx2FTp5gdB04t89/1O/w1cDnyilFU='
+
+
+        headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + serverToken,
+            }
+
+        body = {    
+            "to": devicetoken,
+            "messages": [
+                 {
+                    "type": "flex",
+                    "altText": "MENU",
+                    "contents": {
+                        "type": "bubble",
+                        "size": "giga",
+                        "direction": "ltr",
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "md",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "spacing": "sm",
+                                    "contents": [
+                                        {
+                                            "type": "box",
+                                            "layout": "baseline",
+                                            "contents": [
+                                                {
+                                                    "type": "icon",
+                                                    "url": "https://www.thaitobacco.or.th/th/wp-content/uploads/2015/09/logo-ttm-admin.png",
+                                                    "size": "5xl"
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "type": "box",
+                                            "layout": "vertical",
+                                            "contents": [
+                                                {
+                                                    "type": "text",
+                                                    "align": "end",
+                                                    "text": "MENU",
+                                                    "weight": "bold",
+                                                    "margin": "sm",
+                                                    "size": "3xl",
+                                                    "contents": []
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "Tobacco Authority of Thailand",
+                                    "size": "xxs",
+                                    "color": "#AAAAAA",
+                                    "contents": []
+                                }
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "NEWS",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        },
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "Time & Attendance",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "margin": "xs",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "margin": "xs",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "Doctor Table",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        },
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "Tobacco Club",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "margin": "xs",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "margin": "xs",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "Tobacco Cooperative",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        },
+                                        {
+                                            "type": "button",
+                                            "action": {
+                                                "type": "uri",
+                                                "label": "LOG OUT",
+                                                "uri": "https://liff.line.me/1654967329-5AMQZKN1"
+                                            },
+                                            "color": "#D39D2B",
+                                            "margin": "xs",
+                                            "style": "primary",
+                                            "gravity": "center"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                }      
+            ]
+        
+        }
+        response = requests.post("https://api.line.me/v2/bot/message/push",headers = headers, data=json.dumps(body))
+        print(response.status_code)
+
+        print(response.json())
+
+# ResponsMenu("Uc1e2655638774e42ab8cf38043744cdb")
